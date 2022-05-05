fb81e8186c3ab28e96d94f3231497d01c86da081
entlein <einentlein@gmail.com>
2022-05-05 15:59:38 +0200

    Tutorial 1: add uninstall script

--- pacman-uninstall.sh
+++ pacman-uninstall.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+export KUBECONFIG=~/Downloads/local-2.yaml
+export pacman='u-r6hsy45r2f'
 #kubectl delete -n $pacman -f security/rbac.yaml
 kubectl delete -n $pacman -f security/secret.yaml
 kubectl delete -n $pacman -f deployments/mongo-deployment.yaml
@@ -13,3 +14,7 @@ then
 else
     kubectl delete -n $pacman -f persistentvolumeclaim/mongo-pvc.yaml
 fi
+
+echo "cleaning up your files"
+rm persistentvolumeclaim/mongo-pvc.yaml
+rm security/secret.yaml
